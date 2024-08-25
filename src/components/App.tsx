6fcc9b86a83d2602f90f907d16096f1818141310
Yiphta-lab <148860371+Yiphta-lab@users.noreply.github.com>
2024-08-25 14:52:56 +0200

    Update App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,24 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Demo from "../pages/Demo";
 import Layout from "../pages/Layout";
-import Kernel from "../pages/Kernel";
-import Visualization from "../pages/Visualization";
-import Counter from "../pages/Counter"
 import TextInput from "../pages/TextInput";
-import Timer from "../pages/Timer";
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<Demo />} />
+        
         <Route path="/" element={<Layout />}>
-          <Route path="kernel" element={<Kernel />} />
-         
-          <Route path="visualize" element={<Visualization />} />
-          <Route path="counter" element={<Counter />} />
+    
           <Route path="textinput" element={<TextInput />} />
-          <Route path="timer" element={<Timer />} />
+          
         </Route>
       </Routes>
     </BrowserRouter>
